876fe305dba72d67e0ad036457bbd32e352d51ea
Arthur63 <arthur63@163.com>
2023-11-04 23:05:31 +0800

    Use CHECK instead of assert

--- main.h
+++ main.h
@@ -4,7 +4,6 @@
 #include <time.h>
 #include <stdio.h>
 #include <locale.h>
-#include <assert.h>
 #include <string.h>
 #include <dirent.h>
 #include <stdlib.h>
@@ -16,6 +15,13 @@
 #define BUFF_SIZE 4096
 #define WATCH_SIZE 4096
 #define MASK IN_CLOSE_WRITE
+
+#define CHECK(expr, msg)                                                       \
+    if (!(expr)) {                                                             \
+        fprintf(stderr, "error on line: %d\nexpr: %s\nmsg : %s\n", __LINE__,   \
+                #expr, msg);                                                   \
+        exit(0);                                                               \
+    }
 
 static void add_watch(const char *dpath);
 static void get_time(char *dest, size_t size);
